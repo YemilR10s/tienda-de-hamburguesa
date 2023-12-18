d0b81f8e30f6d0c293684f2b00240f92eaeb28c7
yemilRios <sigifredoyemil14@gmail.com>
2023-12-18 11:03:32 -0500

    ajustes pre responsive

---
+++ main.js
@@ -0,0 +1,9 @@
+function disponible(){
+   let boton =  document.getElementById('botones')
+    
+    boton.addEventListener('click',mostrar())
+    
+}
+function mostrar(){
+    alert("Be patient, it's not available yet")
+}
